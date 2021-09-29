1a7a4723ed6122592ae802c67042dd8b96f9130f
musyahya <mananabdul37654@gmail.com>
2021-09-29 18:55:49 +0700

    register

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => ['required', 'confirmed', Password::min(8)],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('token-name', ['server:update'])->plainTextToken;
+
+        return response()->json([
+            'message' => 'Berhasil register',
+            'data' => $user,
+            'token' => $token
+        ], 200);
+    }
+}
