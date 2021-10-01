b0e02d1bd7bbacd7f75e25daa0d278514b0b8ac0
musyahya <mananabdul37654@gmail.com>
2021-10-01 15:11:25 +0700

    migtaion detail barang

---
+++ database/migrations/2021_10_01_080855_create_detail_barangs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_barang', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('barang_id');
+            $table->integer('harga');
+            $table->integer('durasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_barang');
+    }
+}
